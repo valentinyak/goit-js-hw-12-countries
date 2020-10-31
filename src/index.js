ec84e9295187c8a7cbddec668ce9d0cd26e20325
valentinyak <63512456+valentinyak@users.noreply.github.com>
2020-10-31 15:53:27 +0200

    Добавляет код выполненного ДЗ

--- src/index.js
+++ src/index.js
@@ -1,32 +1,61 @@
-import fetchCountries from './fetchCountries';
+import fetchCountries from './fetch-countries';
 import debounce from 'lodash.debounce';
-import createNotificationError from './createNotificationError';
-import listTemplate from './listTemplate.hbs';
+import createNotificationError from './create-notification-error';
+import listTemplate from './list-template.hbs';
+import countryTemplate from './country-template.hbs';
 
 import '@pnotify/core/dist/BrightTheme.css';
 import './common.css';
 
-const refs = {
-  inputEl: document.querySelector('input'),
-};
-
-refs.inputEl.addEventListener('input', debounce(getContriesFromInput, 500));
+const inputEl = document.querySelector('input');
+inputEl.addEventListener('input', debounce(getContriesFromInput, 500));
 
 function getContriesFromInput() {
-  const serchQuery = fetchCountries(refs.inputEl.value);
-  // serchQuery.then(data => findSerchResaltLength(data));
-  // listTemplate(serchQuery);
+  const serchQuery = fetchCountries(inputEl.value);
+
   serchQuery
     .then(data => {
-      return listTemplate(data);
+      return findSerchResaltLength(data);
     })
-    .then(console.log);
+    .then(markup => {
+      if (markup !== undefined) {
+        createMarkup(markup);
+      }
+    })
+    .catch(error => console.log(error));
 }
 
 function findSerchResaltLength(array) {
+  clearMarkup();
+
+  if (array.status === 404) {
+    return;
+  }
+
   if (array.length > 10) {
     createNotificationError();
   } else if (array.length > 1 && array.length < 10) {
-    listTemplate(array);
+    return listTemplate(array);
+  } else {
+    return countryTemplate(array);
+  }
+}
+
+function createMarkup(markup) {
+  inputEl.insertAdjacentHTML('afterend', markup);
+}
+
+function clearMarkup() {
+  const refs = {
+    countryListEl: document.querySelector('ul.counry-list'),
+    countryArticleEl: document.querySelector('div.country-article'),
+  };
+
+  if (refs.countryListEl) {
+    refs.countryListEl.remove();
+  }
+
+  if (refs.countryArticleEl) {
+    refs.countryArticleEl.remove();
   }
 }
